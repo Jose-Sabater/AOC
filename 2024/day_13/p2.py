b429169b22b50f1858d05d4c808faba73b2b08ce
Jose <jose.sabater@sas.se>
2024-12-18 15:59:40 +0100

    p2

--- 2024/day_13/p2.py
+++ 2024/day_13/p2.py
@@ -7,6 +7,8 @@ from collections import deque
 input_path = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 
 slots = open(input_path).read().split("\n\n")
+added = 10_000_000_000_000
+ratio = added / 100_000  # we select 100_000 for our work
 
 
 # print(slots)
@@ -27,27 +29,36 @@ for slot in slots:
     price_y = int(re.findall(r"Y\=(\d+)", slot)[0])
     A = {"x": ax, "y": ay, "t": 3}
     B = {"x": bx, "y": by, "t": 1}
-    # the optimal is always when you use the largest number first. And then find the largest combination of largest num so that whats left is taken by second largest
-    # deltas
 
-    print(f"Price y {price_y} is smaller than Price x {price_x} ")
+    print(f"Price y {price_y} is smaller than Price x {price_x}")
     small, big = get_smallest(A, B, "y")
-    # print(small, big)
-    # print(min_y)
-    t_l = []
-    for i, num in enumerate(range(0, price_y, small["y"])):
-        if (price_y - num) % big["y"] == 0:
-            # check if the other side also matches
-            n_small_y = i
-            n_big_y = (price_y - num) // big["y"]
 
-            if n_small_y * small["x"] + n_big_y * big["x"] == price_x:
-                tokens = n_small_y * small["t"] + n_big_y * big["t"]
-                t_l.append(tokens)
-    print(t_l)
-    if t_l != []:
-        f_t.append(min(t_l))
+    # Initial floor and remainder
+    floor = added // big["y"]
+    rest = added % big["y"]
+    effective_price_y = price_y + rest  # Start with the remainder added
 
+    # Try decreasing floor if no solution is found
+    while floor >= 0:
+        t_l = []
+        for n_big_y in range(effective_price_y // big["y"] + 1):
+            remaining_price_y = effective_price_y - n_big_y * big["y"]
+            if remaining_price_y % small["y"] == 0:
+                n_small_y = remaining_price_y // small["y"]
+                if n_small_y * small["x"] + n_big_y * big["x"] == price_x:
+                    tokens = n_small_y * small["t"] + n_big_y * big["t"]
+                    t_l.append(tokens)
+
+        if t_l:
+            f_t.append(min(t_l) + floor)
+            break  # Solution found, move to the next slot
+        else:
+            # Adjust floor and effective price
+            floor -= 1
+            effective_price_y -= big["y"]
+
+    if floor < 0:
+        print(f"No solution found for slot: {slot}")
 
 # P2
 # do the same for a 100k and then multiply by the ration 1000000000/100k
